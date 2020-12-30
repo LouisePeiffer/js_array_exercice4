dc10024d5ad7a764e8c5f0f1f645e58a970dcea6
Louise <louisepeiffer@hotmail.com>
2020-12-30 15:24:23 +0100

    oupsies, fini

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+let coucou = ['salut', 'salut 1', 'salut 2']
+console.log(coucou);
+coucou.push('Louise Bad B du 1030')
+console.log(coucou);
+coucou.splice(1, 1, 'Au revoir b')
+console.log(coucou);
+coucou.splice(2, 1, 'ik ben moe')
+console.log(coucou);
